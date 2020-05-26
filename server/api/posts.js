d806b92f365be6f9a1fd9764c15635c6424f8ec5
Carrie Pascale <cpascale43@gmail.com>
2020-05-25 21:09:58 -0400

    feat: error messages & sends all posts

--- server/api/posts.js
+++ server/api/posts.js
@@ -1,11 +1,28 @@
-const router = require('express').Router()
-module.exports = router
+const router = require("express").Router();
+module.exports = router;
+let posts = require("../data");
 
-router.get('/', async (req, res, next) => {
+router.get("/", async (req, res, next) => {
   try {
-    const posts = require('../data')
-    res.json(posts)
+    let { tags, sortBy, direction } = req.query;
+    const validSortBy = ["id", "reads", "likes", "popularity"];
+    const validDirection = ["asc", "desc"];
+
+    // return errors for missing or invalid parameters
+    if (!tags) {
+      return res.status(400).json({ error: "Tags parameter is required" });
+    }
+
+    if (!validSortBy.includes(sortBy.toLowerCase())) {
+      return res.status(400).json({ error: "sortBy parameter is invalid" });
+    }
+
+    if (!validDirection.includes(direction.toLowerCase())) {
+      return res.status(400).json({ error: "direction parameter is invalid" });
+    }
+
+    res.json(posts);
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});

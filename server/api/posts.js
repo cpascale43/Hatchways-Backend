546f3e0c49b1b481b431c3a3b20109ff46bc2031
Carrie Pascale <cpascale43@gmail.com>
2020-05-27 11:11:16 -0400

    finished Part 2

--- server/api/posts.js
+++ server/api/posts.js
@@ -8,47 +8,50 @@ router.get("/", async (req, res, next) => {
   direction = direction ? direction.trim(" ") : "asc";
   const validSortBy = ["id", "reads", "likes", "popularity"];
   const validDirection = ["asc", "desc"];
+  const duplicates = new Set();
 
-  // Return errors for missing or invalid parameters
+  // Send errors for missing or invalid parameters
   if (!tags) {
-    return res.status(400).json({ error: "Tags parameter is required" });
+    res.status(400).json({ error: "Tags parameter is required" });
   }
 
   if (!validSortBy.includes(sortBy.toLowerCase())) {
-    return res.status(400).json({ error: "sortBy parameter is invalid" });
+    res.status(400).json({ error: "sortBy parameter is invalid" });
   }
 
   if (!validDirection.includes(direction.toLowerCase())) {
-    return res.status(400).json({ error: "Direction parameter is invalid" });
+    res.status(400).json({ error: "Direction parameter is invalid" });
   }
 
   // Send result based on the tags provided
   tags = tags.split(",");
-  let response;
   try {
-    let posts = [].concat.apply(
-      [],
-      await Promise.all(
-        tags.map(async (tag) => {
-          response = await axios(
-            `https://hatchways.io/api/assessment/blog/posts?tag=${tag.trim(
-              " "
-            )}&sortBy=${sortBy}&direction=${direction}`
-          );
-          // Removes duplicate results
-          return response.data.posts.filter((post) => {
-            const duplicate = seen.has(post.id);
-            seen.add(post.id);
-            return !duplicate;
-          });
-        })
-      )
-    );
+    let posts = await Promise.all(
+      tags.map(async (tag) => {
+        response = await axios(
+          `https://hatchways.io/api/assessment/blog/posts?tag=${tag.trim(
+            " "
+          )}&sortBy=${sortBy}&direction=${direction}`
+        );
+        // Removes duplicate results
+        return response.data.posts.filter((post) => {
+          const duplicate = duplicates.has(post.id);
+          duplicates.add(post.id);
+          return !duplicate;
+        });
+      })
+    )
 
-    // sort the posts arraylist based on the sortby
+    // Flatten result
+    posts = posts.flat()
+
+    // Sort the posts arr
     posts.sort((a, b) => (a[sortBy] > b[sortBy] ? 1 : -1));
-    // return reverse posts array if direction is desecnding
-    if (direction === "desc") res.json({ posts: posts.reverse() });
+
+    // If direction is descending, return reverse posts arr
+    if (direction === "desc") {
+      posts = posts.reverse()
+    }
 
     res.json({ posts });
   } catch (error) {
